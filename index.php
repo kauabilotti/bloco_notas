edbea7678d7c6736708ea06cff91754aa76c4162
Your Name <you@example.com>
2024-09-30 08:54:22 -0300

    Criei as pastas

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "bloco";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
